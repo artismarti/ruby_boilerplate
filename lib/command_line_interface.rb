b2776ac4daee8926d0eb1d91969fdaa406c13106
artismarti <artismarti@gmail.com>
2019-02-17 22:02:02 +0000

    Adds greeting

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -5,4 +5,10 @@ class CommandLineInterface
   def initialize(vending_machine)
     @machine = vending_machine
   end
+
+  def greet
+    hello = Artii::Base.new font: 'slant'
+    puts hello.asciify('Eat Something')
+  end
+
 end

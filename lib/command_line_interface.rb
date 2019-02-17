bea887957c49e5ee5bd0099fd5c8afed1b849f60
artismarti <artismarti@gmail.com>
2019-02-17 22:09:19 +0000

    Adds intro with menu options

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -11,4 +11,12 @@ class CommandLineInterface
     puts hello.asciify('Eat Something')
   end
 
+  def intro
+    options = { '1' => ' Choose a snack', '2' => ' View cash', 'X' => ' To Exit' }
+    puts 'Welcome to Snacker'
+    puts '---------------------------'
+    options.each do |k, v|
+      puts Rainbow(k).black.underline.bg(:yellow) + " #{v}"
+    end
+  end
 end

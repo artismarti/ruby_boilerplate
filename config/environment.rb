f3173faf11ce639ebb6384ea3c895889d17f9e14
artismarti <artismarti@gmail.com>
2019-02-17 22:11:13 +0000

    Adds config/environment file

---
+++ config/environment.rb
@@ -0,0 +1,9 @@
+require 'bundler'
+Bundler.require
+require 'require_all'
+
+require_all 'lib'
+# require_relative 'product'
+# require_relative 'coin'
+# require_relative 'vending_machine'
+# require_relative '../Gemfile'

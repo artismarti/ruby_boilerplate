65da593771ede58bb79cb55d5e67f30a29d08c86
artismarti <artismarti@gmail.com>
2019-02-18 00:34:42 +0000

    Takes more money from user if due Buys products once user has made selection

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -40,9 +40,6 @@ class CommandLineInterface
       when '2'
         puts Rainbow(' > ').black.underline.bg(:yellow) + 'Cash Available:'
         puts @machine.current_coin_total
-      when 'C'
-        system('clear')
-        intro
       when 'X'
         goodbye = Artii::Base.new font: 'slant'
         puts goodbye.asciify('Bye!')
@@ -50,19 +47,43 @@ class CommandLineInterface
       else
         puts 'These are not the snacks you are looking for. Please input the name of the snack'
       end
-      system('clear')
     end
   end
-
 
   # Accept user choice for product
   def select_product
     puts Rainbow(' > ').black.underline.bg(:yellow) + 'Type your choice:'
     choice = ''
+    coins = false
     while choice
       choice = gets.chomp
-      @machine.check_for_product(choice) == 1 ? buy_product(choice) : [intro, menu_setting]
+      @machine.check_for_product(choice) == 1 ? buy_product(choice, coins) : [intro, menu_setting]
     end
   end
 
+  # If user selects a product that is available, they can buy the product
+  def buy_product(choice, coins)
+    print Rainbow(' > ').black.underline.bg(:yellow) + 'Enter coin value in pennies (seperate by spaces):'
+    puts '2 20 10'
+    coins ||= gets.chomp.split.map(&:to_i)
+    change_due = @machine.paid_amount_sufficient?(choice, @machine.inspect_input_coins(coins, @machine.coins[0].class::DENOMINATIONS))
+    take_more_money(coins, change_due, choice) if change_due < 0
+    puts 'Thanks for your purchase, snacker!'
+    [intro, menu_setting]
+  end
+
+  def take_more_money(coins, change_due, choice)
+    puts "Please pay #{change_due} pennies more:"
+    prod_price = coins.reduce(0) { |s, n| s += n } + change_due
+    more_coins = gets.chomp.split.map(&:to_i)
+    coins << more_coins
+    coins = coins.flatten
+    change_due -=  more_coins.reduce(0) { |s, n| s += n }
+    if prod_price - coins.reduce(0) { |s, n| s += n } > 0
+      take_more_money(coins, change_due, choice)
+    else
+      coins << more_coins
+      coins = coins.flatten
+    end
+  end
 end
